efa8815cced853b4c1dd883183e46cea57116785
Ranulfo Mascari Neto <ranulfomascarineto@Ranulfos-MacBook-Air.local>
2022-04-22 19:55:23 -0300

    first commit

---
+++ passagens/views.py
@@ -0,0 +1,26 @@
+from urllib import request
+from django.shortcuts import render
+from passagens.forms import PassagemForms, PessoaForms
+
+def index(request):
+    form = PassagemForms()
+    pessoa_form = PessoaForms()
+    contexto = {
+        'form':form,
+        'pessoa_form':pessoa_form,
+    }
+    return render(request, 'index.html', contexto)
+
+def revisao_consulta(request):
+    if request.method == 'POST':
+        form = PassagemForms(request.POST)
+        pessoa_form = PessoaForms(request.POST)
+        contexto = {
+                'form':form,
+                'pessoa_form':pessoa_form,
+        }
+        if form.is_valid():
+            return render(request, 'minha_consulta.html', contexto)
+        else:
+            print('Form inválido')
+            return render(request, 'index.html', contexto)
